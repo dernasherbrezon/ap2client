0d21d51c22cc0b214e752097901f5540b6c98d5c
dernasherbrezon <rodionovamp@mail.ru>
2018-04-02 14:57:19 +0100

    initial travis and debian infra + fswatch for changes in wpa_supplicant

---
+++ build-debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+DATE=`date +%s`
+
+echo "Version: ${DATE}" >> debian/DEBIAN/control
+cp src/*.sh /bin/ap2client
+
+dpkg-deb --build debian ap2client-${DATE}.deb
+
+openssl aes-256-cbc -K $encrypted_ace8744cfeee_key -iv $encrypted_ace8744cfeee_iv -in codesigning.asc.enc -out codesigning.asc -d
+gpg --fast-import codesigning.asc
+
+deb-s3 upload -a armhf -c r2cloud --access-key-id=${AWS_ACCESS_KEY} --secret-access-key=${AWS_SECRET_ACCESS_KEY}  -m main --sign=A5A70917 --gpg-options="--passphrase ${GPG_PASSPHRASE} --digest-algo SHA256" --bucket r2cloud *.deb
---
+++ test/test_core.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+workingdir="./bash_unit"
+scriptLocation=../src/ap2client.sh
+
+test_success() {
+	echo "success"
+}
