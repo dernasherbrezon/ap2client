0de0b75cf76563984de053b9a0e255b3c100a7d9
dernasherbrezon <rodionovamp@mail.ru>
2018-04-03 20:48:45 +0100

    wpaCliMonitor

--- src/wpaCliMonitor.sh
+++ src/wpaCliMonitor.sh
@@ -3,3 +3,49 @@
 cd $(dirname "$0") 
 . ./common.sh
 
+INTERFACE=${1}
+MAX_RETRY=5
+RETRY_TIMEOUT_SECONDS=15
+CONNECTED=0
+
+function onConnected() { 
+	log "wifi connected"
+	CONNECTED=1
+	dhclient ${INTERFACE}
+	# ensure hostapd stopped
+	# it won't be possible to connect in client mode
+	# and remain access point
+	systemctl stop hostapd
+	return 0
+}
+
+function onDisconnected() {
+	log "wifi disconnected"
+	CONNECTED=0
+	# disconnected might be caused by auth failure
+	# however there is no such event from wpa_cli, 
+	# so retry even if connection definitely won't be established
+	# currently responsiveness is MAX_RETRY * RETRY_TIMEOUT_SECONDS
+	local retry=0
+	until [ ${retry} -ge ${MAX_RETRY} ] || [ ${CONNECTED} -eq 1 ]
+	do
+		log "reconnect...attempt ${retry}"
+		killall -HUP wpa_supplicant
+		((retry++))
+		sleep ${RETRY_TIMEOUT_SECONDS}
+	done
+	if [ ${CONNECTED} -eq 0 ]; then
+		log "not reconnected within timeout. init access point mode"
+		systemctl start hostapd
+	fi
+	return 0
+}
+
+case "$2" in
+    CONNECTED)
+        onConnected
+        ;;
+    DISCONNECTED)
+        onDisconnected
+        ;;
+esac
