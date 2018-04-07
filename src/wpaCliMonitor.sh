5408ef5a1a06053681efc4845f4f6be01475ddc5
dernasherbrezon <rodionovamp@mail.ru>
2018-04-07 23:22:35 +0100

    network configuration

--- src/wpaCliMonitor.sh
+++ src/wpaCliMonitor.sh
@@ -6,11 +6,9 @@ cd $(dirname "$0")
 INTERFACE=${1}
 MAX_RETRY=5
 RETRY_TIMEOUT_SECONDS=15
-CONNECTED=0
 
 function onConnected() { 
-	log "wifi connected"
-	CONNECTED=1
+	log "wifi connected: `iwconfig ${INTERFACE} | grep ${INTERFACE}`"
 	local IS_MASTER_MODE=`iwconfig ${INTERFACE} | grep "Mode:Master" | wc -l`
 	if [ ${IS_MASTER_MODE} -eq 1 ]; then
 		log "connected as access point"
@@ -27,21 +25,22 @@ function onConnected() {
 }
 
 function onDisconnected() {
-	log "wifi disconnected"
-	CONNECTED=0
+	log "wifi disconnected: `iwconfig ${INTERFACE} | grep ${INTERFACE}`"
+	local connected=0
 	# disconnected might be caused by auth failure
 	# however there is no such event from wpa_cli, 
 	# so retry even if connection definitely won't be established
 	# currently responsiveness is MAX_RETRY * RETRY_TIMEOUT_SECONDS
 	local retry=0
-	until [ ${retry} -ge ${MAX_RETRY} ] || [ ${CONNECTED} -eq 1 ]
+	until [ ${retry} -ge ${MAX_RETRY} ] || [ ${connected} -eq 1 ]
 	do
 		log "reconnect...attempt ${retry}"
 		killall -HUP wpa_supplicant
 		((retry++))
 		sleep ${RETRY_TIMEOUT_SECONDS}
+		connected=`iwconfig ${INTERFACE} | grep ESSID:\" | wc -l`
 	done
-	if [ ${CONNECTED} -eq 0 ]; then
+	if [ ${connected} -eq 0 ]; then
 		log "not reconnected within timeout. init access point mode"
 		systemctl start dnsmasq
 		systemctl start hostapd
