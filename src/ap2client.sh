645c054892c6df71e481c613f4c6165ec71c759f
dernasherbrezon <rodionovamp@mail.ru>
2018-04-29 23:11:58 +0100

    setup files + fix issues with resolv.conf

--- src/ap2client.sh
+++ src/ap2client.sh
@@ -5,12 +5,5 @@
 cd $(dirname "$0") 
 . ./common.sh
 
-function _term() {
-	log "caught termination. stop child process"
-	kill $(ps -s $$ -o pid=) 
-}
-
-trap _term SIGTERM
-
 `pwd`/configMonitor.sh /etc/wpa_supplicant/wpa_supplicant.conf &
 wpa_cli -i wlan0 -G 1 -a `pwd`/wpaCliMonitor.sh
