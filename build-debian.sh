26d0f2f65e244413624d31799289672e11ca460b
dernasherbrezon <rodionovamp@mail.ru>
2018-05-01 16:32:09 +0100

    fix gpg keys

--- build-debian.sh
+++ build-debian.sh
@@ -11,7 +11,8 @@ chmod +x debian/DEBIAN/postinst
 
 dpkg-deb --build debian ap2client-${DATE}.deb
 
-openssl aes-256-cbc -K $encrypted_ace8744cfeee_key -iv $encrypted_ace8744cfeee_iv -in codesigning.asc.enc -out codesigning.asc -d
-gpg --fast-import codesigning.asc
+openssl aes-256-cbc -K $encrypted_806ce29048dc_key -iv $encrypted_806ce29048dc_iv -in codesigning2.asc.enc -out codesigning2.asc -d
+
+gpg --fast-import codesigning2.asc
 
 deb-s3 upload -a armhf -c r2cloud --access-key-id=${AWS_ACCESS_KEY} --secret-access-key=${AWS_SECRET_ACCESS_KEY}  -m main --sign=A5A70917 --gpg-options="--passphrase ${GPG_PASSPHRASE} --digest-algo SHA256" --bucket r2cloud *.deb
